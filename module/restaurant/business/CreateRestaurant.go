12c795b5d5a924a3c7e02e61570c7b6b9679449d
VoAnhBen <benva.ce190709@gmail.com>
2025-12-02 15:12:49 +0700

    create file .env and launch.json

--- module/restaurant/business/CreateRestaurant.go
+++ module/restaurant/business/CreateRestaurant.go
@@ -12,11 +12,11 @@ type createRestaurantStore interface {
 type createRestaurantBusiness struct {
 	store createRestaurantStore
 }
-
+// NewCreateRestaurantBusiness use interface
 func NewCreateRestaurantBusiness(store createRestaurantStore) *createRestaurantBusiness {
 	return &createRestaurantBusiness{store: store}
 }
-
+// CreateRestaurant login interface
 func (biz *createRestaurantBusiness) CreateRestaurant(ctx context.Context, data *restaurantmodel.RestaurantCreate) error {
 	if data.Name == "" {
 		return errors.New("name cannot be empty")
