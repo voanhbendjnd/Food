1cc7f2dd9ae13cce3f640b3de28cbf135d0c7e03
VoAnhBen <benva.ce190709@gmail.com>
2025-12-04 19:18:25 +0700

    new file app_error.go for feature handler errors

---
+++ common/app_error.go
@@ -0,0 +1,85 @@
+package common
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type AppError struct {
+	StatusCode int    `json:"status_code"`
+	RootErr    error  `json:"-"`
+	Message    string `json:"message"`
+	Log        string `json:"log"`
+	Key        string `json:"error_key"`
+}
+
+func NewErrorResponse(root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusBadRequest,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewFullErrorResponse(statusCode int, root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: statusCode,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+// 401
+func NewUnauthorized(root error, msg, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusUnauthorized,
+		RootErr:    root,
+		Message:    msg,
+		Key:        key,
+	}
+}
+
+func NewCustomError(root error, msg, key string) *AppError {
+	if root != nil {
+		return NewErrorResponse(root, msg, root.Error(), key)
+	}
+	return NewErrorResponse(errors.New(msg), msg, msg, key)
+}
+
+func (e *AppError) RootError() error {
+	if err, ok := e.RootErr.(*AppError); ok {
+		return err.RootError()
+	}
+	return e.RootErr
+}
+func (e *AppError) Error() string {
+	return e.RootError().Error()
+}
+
+func ErrDB(err error) *AppError {
+	return NewErrorResponse(err, "something went wrong with DB", err.Error(), "DB_ERROR")
+}
+func ErrInvalidRequest(err error) *AppError {
+	return NewErrorResponse(err, "invalid request", err.Error(), "ErrInvalidRequest")
+}
+
+func ErrINternal(err error) *AppError {
+	return NewFullErrorResponse(http.StatusInternalServerError, err, "something went wrong in the server", err.Error(), "ErrInternal")
+}
+
+func ErrCannotListEntity(entity string, err error) *AppError {
+	return NewCustomError(
+		err,
+		fmt.Sprintf("Cannot list %s", strings.ToLower(entity)),
+		fmt.Sprintf("ErrCannotList %s", entity),
+	)
+}
+func ErrCannotDeletedEntity(entity string, err error) *AppError {
+	return NewCustomError(err, "cannot delete entity "+entity, "SERVER_ERROR")
+}
